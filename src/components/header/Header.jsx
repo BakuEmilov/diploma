db01befbb79aaba9088a9198ab9a3badae8f15b8
BakuEmilov <emilovbaku530@gmail.com>
2023-04-08 18:15:52 +0600

    finished navigating

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import css from './Header.module.css';
+import React from "react";
+import css from "./Header.module.css";
+import Logo from "../../assets/nike.png";
+import Cart from "../../assets/cart.png";
 
 const Header = () => {
   return (
     <div className={css.container}>
       <div className={css.logo}>
-        logo
+        <img src={Logo} alt="" />
+        <span>nike</span>
       </div>
 
       <div className={css.right}>
         <div className={css.menu}>
-
+          <ul className={css.menu}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contacts</li>
+            <li>Delivery</li>
+          </ul>
         </div>
-        <input type="" className={css.search} />
 
-        <span>PNG icon  of cart</span>
+          <input type="" className={css.search} placeholder='Search' />
+          <img className={css.cart} src={Cart} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
