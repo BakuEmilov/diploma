db1262d4128569ffbcdf1d77484a206c14c6ed47
BakuEmilov <emilovbaku530@gmail.com>
2023-04-09 17:48:54 +0600

    prepared Hero data for styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import css from './Hero.module.css';
+import Arrow from "../../assets/arrow.png";
+import Cart2 from "../../assets/cart-2.png";
+import HeroImg from "../../assets/hero.png";
+
 const Hero = () => {
   return(
    <div className={css.container}>
@@ -17,8 +21,29 @@ const Hero = () => {
 
      {/*middle side hero image*/}
      <div className={css.wrapper}>
-      <div className={css.blueCircle}></div>
+      <div className={css.blueCircle}>
+        <img src={HeroImg} alt="" width={500} />
+        <div className="block">
+          <img className={css.cart2} src={Cart2} alt="" />
+          <div className={css.singup}>
+            <span>sign up</span>
+              <img className={css.arrow} src={Arrow} alt="" />
+          </div>
+        </div>
+      </div>
      </div>
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>1.7m</span>
+          <span>Monthly Traffic</span>
+        </div>
+
+        <div className={css.cunstomers}>
+          <span>100k</span>
+          <span>Happy Customers</span>
+        </div>
+      </div>
+
     </div>
    </div>
   )
