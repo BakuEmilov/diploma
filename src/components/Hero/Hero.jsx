8e5687279226be0c6336d6d1c31adcdf157b2112
BakuEmilov <emilovbaku530@gmail.com>
2023-04-10 06:17:29 +0600

    remake folder hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,51 +1,19 @@
 import React from "react";
-import css from './Hero.module.css';
+import css from "./Hero.module.css";
 import Arrow from "../../assets/arrow.png";
 import Cart2 from "../../assets/cart-2.png";
 import HeroImg from "../../assets/hero.png";
 
 const Hero = () => {
-  return(
-   <div className={css.container}>
-
-
-    {/*left side*/}
-    <div className={css.h_sides}>
-      <span>#1 clothing brand</span>
-      
-     <div className={css.text2}>
-      <span>Quality clothes for you</span>
-      <span>by Nike</span>
-     </div>
-
-
-     {/*middle side hero image*/}
-     <div className={css.wrapper}>
-      <div className={css.blueCircle}>
-        <img src={HeroImg} alt="" width={500} />
-        <div className="block">
-          <img className={css.cart2} src={Cart2} alt="" />
-          <div className={css.singup}>
-            <span>sign up</span>
-              <img className={css.arrow} src={Arrow} alt="" />
-          </div>
-        </div>
-      </div>
-     </div>
+  return (
+    <div className={css.container}>
+      {/*left side*/}
       <div className={css.h_sides}>
-        <div className={css.traffic}>
-          <span>1.7m</span>
-          <span>Monthly Traffic</span>
-        </div>
+        <span>skin protection cream</span>
 
-        <div className={css.cunstomers}>
-          <span>100k</span>
-          <span>Happy Customers</span>
-        </div>
+        <div className={css.text2}></div>
       </div>
-
     </div>
-   </div>
-  )
-}
+  );
+};
 export default Hero;
