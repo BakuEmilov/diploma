482e902f57005c51364481692f5a8bff52d79155
BakuEmilov <emilovbaku530@gmail.com>
2023-04-10 17:31:15 +0600

    wanted to redo everything and deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
-import Hero from './components/Hero/Hero';
-import Slider from './components/Slider/Slider';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Hero />
-      <Slider />
     </div>
   );
 }
