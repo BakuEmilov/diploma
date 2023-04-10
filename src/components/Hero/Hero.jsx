e45f29627f88cbdf870f991cabe503e59df6612d
BakuEmilov <emilovbaku530@gmail.com>
2023-04-10 07:11:03 +0600

    wrote html for hero center and right side

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import css from "./Hero.module.css";
+import HeroImg from "../../assets/hero.png";
+import { BsArrowRight } from "react-icons/bs";
+
+
 
 
 const Hero = () => {
@@ -17,9 +21,38 @@ const Hero = () => {
           </span>
         </div>
       </div>
+
       {/*middle side hero image*/}
-      
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt="" width={500} />
+        <div className={css.cart2}>
+           
+
+          <div className={css.signup}>
+            <span>Best Signup Offers</span>
+
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/*right side*/}
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>1.7m</span>
+          <span>Monthly Traffic</span>
+        </div>
+
+        <div className={css.customers}>
+          <span>100k</span>
+          <span>Happy Customers</span>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Hero;
