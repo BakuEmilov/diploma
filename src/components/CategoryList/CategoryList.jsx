dbc9181892c109ff1bded4b4b7b3c0cf15fd315f
BakuEmilov <emilovbaku530@gmail.com>
2023-04-13 14:30:38 +0600

    added 2 products via firebase

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 import { useContext } from "react";
+import "./CategoryList.css"
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
