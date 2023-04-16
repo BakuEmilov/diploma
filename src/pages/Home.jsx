ccf34f71783854926dcbe3085f4e31fde6123ad9
BakuEmilov <emilovbaku530@gmail.com>
2023-04-16 15:04:15 +0600

    decided to use another swiper method

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,7 @@
-import { Swiper , SwiperSlide } from 'swiper/react'
 
-import 'swiper/swiper.css'
 
 export default function Home() {
   return (
-      <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+      
   );
 }
