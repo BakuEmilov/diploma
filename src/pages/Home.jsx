d40c6c0531a502592294df419f420bf12dbf1dae
BakuEmilov <emilovbaku530@gmail.com>
2023-04-16 15:14:37 +0600

    import Swiper, SwiperSlide.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 
-export default function Home() {
-  return (
-      
-  );
+const Slider = () => {
+  <div className="pictures">
+     <Swiper>
+       <SwiperSlide>1</SwiperSlide>
+       <SwiperSlide>2</SwiperSlide>
+       <SwiperSlide>3</SwiperSlide>
+     </Swiper>
+  </div>
 }
+
+export default Slider;
