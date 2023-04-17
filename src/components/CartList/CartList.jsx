cb68d5b2424a2ea92ec75ceb3811581320caaad5
BakuEmilov <emilovbaku530@gmail.com>
2023-04-17 15:56:56 +0600

    added button and input for CartList

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
-import "./CartList.css"
+import "./CartList.css";
 
 export default function () {
   //получить список товаров и корзинку
@@ -9,40 +9,36 @@ export default function () {
 
   function onQuantityChange(product, qty) {
     setCart({
-    ...cart,
-    [product.id]: qty,
-    })
+      ...cart,
+      [product.id]: qty,
+    });
   }
-  function onItemRemove(product){
-    const newCart = {...cart};
+  function onItemRemove(product) {
+    const newCart = { ...cart };
     delete newCart[product.id];
     setCart(newCart);
   }
-  
+
   //
   const productIds = Object.keys(cart);
 
   const output = products
-  .filter((product) => productIds.includes(product.id))
-  .map((product) => (
-   <div className="Caritem" key={product.id}>
-    <img src={product.picture} alt={product.name} />
-    <Link to={"/product/" + product.slug}>{product.name}</Link>
-    {cart[product.id]}
-    <input type="number"
-     value={cart[product.id]} 
-     min={1} 
-     onChange={(event) => onQuantityChange
-      (product,+event.target.value)}
-       />
-    <span>{cart[product.id] * product.price} $</span>
-    <button onClick={() => onItemRemove(product)}>Remove</button>
-   </div>
-  ));
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+      <div className="Caritem" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        {cart[product.id]}
+        <input
+          type="number"
+          value={cart[product.id]}
+          min={1}
+          onChange={(event) => onQuantityChange(product, +event.target.value)}
+        />
+        <span>{cart[product.id] * product.price} $</span>
+        <button onClick={() => onItemRemove(product)}>Remove</button>
+      </div>
+    ));
 
-  return (
-    <div className="CartList">
-      {output}
-    </div>
-  )
+  return <div className="CartList">{output}</div>;
 }
