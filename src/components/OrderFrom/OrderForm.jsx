87f77a9dfc6ea5e3c6e30a839d97396f8f605207
BakuEmilov <emilovbaku530@gmail.com>
2023-04-24 15:09:29 +0600

    changed user

--- src/components/OrderFrom/OrderForm.jsx
+++ src/components/OrderFrom/OrderForm.jsx
@@ -6,13 +6,16 @@ import { AppContext } from "../../App";
 import { useNavigate } from "react-router-dom";
 
 export default function OrderForm() {
-  const { cart, setCart } = useContext(AppContext);
+  const { cart, setCart, user } = useContext(AppContext);
   const navigate = useNavigate();
 
   if (Object.keys(cart).length === 0) {
     return "Your cart is empty.";
   }
 
+  if (!user) {
+    return "Please login to create an order"
+  }
   function onFormSubmit(event) {
     event.preventDefault();
 
@@ -21,7 +24,7 @@ export default function OrderForm() {
     addDoc(ordersCollection, {
       name: formData.get("name"),
       phone: formData.get("phone"),
-      email: formData.get("email"),
+      user: user.uid,
       address: formData.get("address"),
       cart: cart,
     }).then((doc) => {
@@ -38,9 +41,6 @@ export default function OrderForm() {
       </label>
       <label>
         Phone: <input type="tel" name="phone" required />
-      </label>
-      <label>
-        Email: <input type="email" name="email" required />
       </label>
       <label>
         Address: <input type="text" name="address" required />
