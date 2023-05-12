50e9765cecc4ffbd57ecc594c7eb3526441e5a6f
BakuEmilov <emilovbaku530@gmail.com>
2023-05-12 10:04:26 +0600

    start making footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-3"></div>
+          <div className="col-md-3"></div>
+          <div className="col-md-3"></div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
