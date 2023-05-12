6dda5c633086afb9a6015bf7224a63bf6bfb99d2
BakuEmilov <emilovbaku530@gmail.com>
2023-05-12 10:06:43 +0600

    filled in the first column of the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,26 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="row">
-          <div className="col-md-3"></div>
+          <div className="col-md-3">
+            <h3>Site Map</h3>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/services">Our Services</Link>
+              </li>
+              <li>
+                <Link to="/blog">Blog</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
           <div className="col-md-3"></div>
           <div className="col-md-3"></div>
           <div className="col-md-3"></div>
