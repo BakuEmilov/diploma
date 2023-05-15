559293226bec856554db3f6a1afb3d525047f7e3
BakuEmilov <emilovbaku530@gmail.com>
2023-05-15 18:37:49 +0600

    change style OrderForm

--- src/components/OrderFrom/OrderForm.jsx
+++ src/components/OrderFrom/OrderForm.jsx
@@ -36,7 +36,6 @@ export default function OrderForm() {
 
   return (
     <form className="OrderForm" onSubmit={onFormSubmit}>
-      <h2>Create an order</h2>
       <label>
         Name: <input type="text" name="name" required />
       </label>
@@ -49,15 +48,14 @@ export default function OrderForm() {
       <button>
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="29"
-          height="20"
+          width="30"
+          height="30"
           fill="currentColor"
-          class="bi bi-send-plus-fill"
+          class="bi bi-cart-check-fill"
           viewBox="0 0 16 16"
-          color="white"
+          
         >
-          <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 1.59 2.498C8 14 8 13 8 12.5a4.5 4.5 0 0 1 5.026-4.47L15.964.686Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
-          <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z" />
+          <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708z" />
         </svg>
       </button>
     </form>
