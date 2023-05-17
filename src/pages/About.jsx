8059abbcd40070eaab7aa1f0d1f2103dbf54b760
BakuEmilov <emilovbaku530@gmail.com>
2023-05-17 15:10:22 +0600

    added animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,7 @@
 export default function About() {
   return (
     <div className="About">
-      <h1>About</h1>
-      <p>This is my home page</p>
+     <div className="Nike">Nike JR!</div>
     </div>
   );
 }
