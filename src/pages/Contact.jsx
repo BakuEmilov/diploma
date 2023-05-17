84beb66673d8041f3514575016c51dd205161839
BakuEmilov <emilovbaku530@gmail.com>
2023-05-17 18:16:46 +0600

    added a map for contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+
+
 export default function Contact() {
   return (
     <div className="Contact">
-      <h1>Contact</h1>
-      <p>This is my home page</p>
+      <p>
+        jgyukuku
+      </p>
+       <div className="map">
+       <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d567.077917627546!2d78.41206868975347!3d42.49926619280281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1684324310064!5m2!1sru!2skg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+       </div>
     </div>
   );
 }
