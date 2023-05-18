5aa826ba7799361467d11d64c951872d9ccf7023
BakuEmilov <emilovbaku530@gmail.com>
2023-05-18 14:23:12 +0600

    we begin to supplement Home with texts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="home-text">
-        <h1></h1>
+        <h1>Welcome to Nike</h1>
       </div>
       <div className="video">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/PZIqV7wNyyU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
