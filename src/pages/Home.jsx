0826f4b005206acdc850d194061e4b04d7113479
BakuEmilov <emilovbaku530@gmail.com>
2023-05-18 15:17:47 +0600

    finished making home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="home-text">
-        <h1>Welcome to Nike</h1>
+        <h3>Welcome to Nike</h3>
         <p>
         Less talk, more action. Let's start buying branded Nike clothing at very good prices.
         </p>
+        <button className="home-btn">
+          let's GO
+        </button>
       </div>
       <div className="video">
       <iframe width="560" height="315" src="https://www.youtube.com/embed/PZIqV7wNyyU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
