16fa2d5ac05fc1b169b910958fff36d91302583a
BakuEmilov <emilovbaku530@gmail.com>
2023-05-18 23:05:52 +0600

    changed the style of CartList

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 
 export default function About() {
   return (
@@ -7,7 +8,7 @@ export default function About() {
       <p>The most high-quality and stylish clothing available only with us.</p>
      </div>
      <div className="about-photo">
-      <img src="https://png.pngtree.com/png-vector/20211003/ourmid/pngtree-shoes-sneakers-illustration-sketch-png-image_3967173.png" alt="" />
+      
      </div>
     </div>
   );
