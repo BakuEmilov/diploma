76197867c40e8466069edd1995a5e68c097c7651
BakuEmilov <emilovbaku530@gmail.com>
2023-05-18 17:52:55 +0600

    added number for contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,11 +5,13 @@ export default function Contact() {
   return (
     <div className="Contact">
       <div className='text'>
-        <h1></h1>
+        <h1>Contact us</h1>
+        <p>If you have any questions, please feel free to give us a call:</p>
+        <a href="tel:+996 707 763 896">+996 707 763 896</a>
+
       </div>
        <div className="map">
        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d567.077917627546!2d78.41206868975347!3d42.49926619280281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2skg!4v1684324310064!5m2!1sru!2skg" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
        </div>
     </div>
   );
