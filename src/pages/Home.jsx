b6f8898d5dc37d9a98368104a30b45410320de9f
BakuEmilov <emilovbaku530@gmail.com>
2023-05-18 14:22:13 +0600

    added video for home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <div className="Home">
-      <h1>Home</h1>
-      <p>This is my home page</p>
+      <div className="home-text">
+        <h1></h1>
+      </div>
+      <div className="video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/PZIqV7wNyyU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
     </div>
   );
 }
