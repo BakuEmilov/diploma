2dc662db4341361148c582ade288de40542d6c54
BakuEmilov <emilovbaku530@gmail.com>
2023-05-19 14:20:24 +0600

    changed class to className

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,9 @@ export default function Home() {
           height="315"
           src="https://www.youtube.com/embed/PZIqV7wNyyU"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
