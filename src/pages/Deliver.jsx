2e0471c8b045d03bfeb449ea5969fd20ac82386c
BakuEmilov <emilovbaku530@gmail.com>
2023-05-22 14:38:56 +0600

    wrote code for deliver, skeleton

--- src/pages/Deliver.jsx
+++ src/pages/Deliver.jsx
@@ -1,31 +1,54 @@
-// Deliver.js
-import React from 'react';
+import React from "react";
 
-const Deliver = () => {
+const Delivery = () => {
   return (
-    <div>
-      <h1>Страница Deliver</h1>
-      <p>Здесь можно добавить описание страницы Deliver.</p>
-      
-      {/* Пример формы */}
-      <form>
-        <label htmlFor="name">Имя:</label>
-        <input type="text" id="name" />
-        
-        <label htmlFor="address">Адрес:</label>
-        <input type="text" id="address" />
-        
-        <button type="submit">Отправить</button>
-      </form>
-      
-      {/* Пример списка */}
-      <ul>
-        <li>Пункт 1</li>
-        <li>Пункт 2</li>
-        <li>Пункт 3</li>
-      </ul>
+    <div className="delivery-container">
+      <h2 className="delivery-heading">Fast and Reliable Delivery</h2>
+      <p className="delivery-text">
+        At Nike Shop, we offer quick and dependable delivery services to ensure
+        you receive your products in no time.
+      </p>
+      <p className="delivery-text">
+        Whether you're purchasing sneakers, apparel, or accessories, our
+        dedicated delivery team works tirelessly to deliver your order securely
+        to your doorstep.
+      </p>
+      <p className="delivery-text">
+        Enjoy hassle-free shopping with our streamlined delivery process and
+        stay updated with real-time tracking information for your package.
+      </p>
+      <p className="delivery-text">
+        Experience the convenience of shopping at Nike Shop today and receive
+        your items with speed and efficiency.
+      </p>
+      <div className="delivery-info">
+        <div className="delivery-option">
+          <i className="fas fa-shipping-fast"></i>
+          <h3 className="delivery-option-heading">Fast Delivery</h3>
+          <p className="delivery-option-text">
+            Get your order delivered quickly and efficiently with our expedited
+            shipping options.
+          </p>
+        </div>
+        <div className="delivery-option">
+          <i className="fas fa-shield-alt"></i>
+          <h3 className="delivery-option-heading">Secure Delivery</h3>
+          <p className="delivery-option-text">
+            Rest assured that your package will be delivered securely and
+            protected throughout the shipping process.
+          </p>
+        </div>
+        <div className="delivery-option">
+          <i className="fas fa-box"></i>
+          <h3 className="delivery-option-heading">Track Your Order</h3>
+          <p className="delivery-option-text">
+            Easily track your order in real-time and stay updated on its
+            delivery status.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Deliver;
+export default Delivery;
