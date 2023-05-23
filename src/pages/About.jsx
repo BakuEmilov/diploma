e3aa9ce9cc845a414059392c3e9620cecc71f9cd
BakuEmilov <emilovbaku530@gmail.com>
2023-05-23 22:59:48 +0600

    filled in About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React from 'react';
 
-export default function About() {
+const AboutUs = () => {
   return (
-    <div className="About">
-     <div className="about-text">
-      <h1>About Us</h1>
-      <p>The most high-quality and stylish clothing available only with us.</p>
-     </div>
-     <div className="about-photo">
-      
-     </div>
+    <div className="about-us-container">
+      <h2 className="about-us-heading">About Us</h2>
+      <p className="about-us-description">
+        Welcome to Nike Shop, your ultimate destination for trendy and high-quality sportswear. We
+        are passionate about providing athletes and sports enthusiasts with top-notch products to
+        enhance their performance and style.
+      </p>
+      <p className="about-us-mission">
+        Our mission is to inspire and motivate individuals to reach their fitness goals by offering
+        a wide range of athletic apparel, footwear, and accessories. We believe that everyone has
+        the potential to push their limits and excel in their chosen sports.
+      </p>
+      <p className="about-us-values">
+        At Nike Shop, we value innovation, quality, and customer satisfaction. We work closely with
+        athletes, designers, and experts to create products that combine cutting-edge technology,
+        comfort, and style. Your satisfaction is our top priority, and we strive to provide
+        exceptional customer service and a seamless shopping experience.
+      </p>
+      <p className="about-us-visit">Visit us today and unleash your potential with Nike Shop!</p>
     </div>
   );
-}
+};
+
+export default AboutUs;
