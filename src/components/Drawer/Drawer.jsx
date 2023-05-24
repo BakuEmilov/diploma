d62f91fc05679fead6ffbf3304d1efc06888c867
BakuEmilov <emilovbaku530@gmail.com>
2023-05-24 17:17:36 +0600

    made burger navigation auto-closing

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -2,6 +2,14 @@ import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
 import "./Drawer.css"
 
+let menuBtn = document.querySelectorAll('.NavItem a')
+let menu = document.querySelector('.Drawer')
+menuBtn.forEach(el => {
+    el.addEventListener('click', function(){
+        menu.classList.remove('open')
+    })
+})
+
 function Drawer({ open, toggle }) {
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
 
